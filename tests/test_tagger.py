b47dfe7613d21a4a87f788170b3ac5845ee39d38
Julien Marin <marin.julien@etu.umontpellier.fr>
2018-03-15 12:22:51 +0100

    ajout test unitaire tagger

---
+++ tests/test_tagger.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+# ref : https://docs.pytest.org/en/latest/
+# ref : https://docs.pytest.org/en/latest/pythonpath.html
+
+# pip3 install --user pytest
+# pour lancer : deux solutions (depuis racine du dossier)
+#   - py.test-3 (lancer tous les tests)
+#   - python3 -m pytest ./tests/test_tagger.py "-s"
+
+import pretraitement
+import treetaggerwrapper
+
+def test_poncutation_point():
+    '''Test s'il n'y a pas de problème de taggage (à cause de la
+ponctuation notamment)'''
+    data = "this is a tests.that could bug"  # Générait un NotTag(tests.that)
+    formatted = pretraitement.formattage(data)
+    tags = pretraitement.extract_tags(formatted)
+    assert all( (type(tag) is treetaggerwrapper.Tag) for tag in tags)
+
+# TODO
+def test_ponct():
+    ''' TRACE PROBLEME
+    PROBLEM:  NotTag(what='<repurl text="http://www." />')
+    PROBLEM:  NotTag(what='<repurl text="http://www." />')
+    PROBLEM:  NotTag(what='<repurl text="http://www." />')
+    PROBLEM:  NotTag(what='<repurl text="http://www." />')
+    PROBLEM:  NotTag(what='<repurl text="news:" />')
+    PROBLEM:  NotTag(what='<repurl text="http://imdb." />')
+    PROBLEM:  NotTag(what='<repurl text="file:" />')
+    PROBLEM:  NotTag(what='<repurl text="http://skeptico." />')
+    PROBLEM:  NotTag(what='<repurl text="http://www." />')
+    PROBLEM:  NotTag(what='<repurl text="http://www." />')
+    PROBLEM:  NotTag(what='<repurl text="http://www." />')
+    PROBLEM:  NotTag(what='<repurl text="http://en." />')
+    PROBLEM:  NotTag(what='<repurl text="http://memory-alpha." />')
+    PROBLEM:  NotTag(what='<repurl text="http://www." />')
+    PROBLEM:  NotTag(what='<repurl text="http://www." />')
+    PROBLEM:  NotTag(what='<repurl text="http://www." />')
+    PROBLEM:  NotTag(what='<repurl text="http://www." />')
+    PROBLEM:  NotTag(what='<repurl text="http://www." />')
+    PROBLEM:  NotTag(what='<repurl text="http://www." />')
+    PROBLEM:  NotTag(what='<repurl text="http://ferdinandvongalitzien." />')
+    PROBLEM:  NotTag(what='<repurl text="http://www." />')
+    PROBLEM:  NotTag(what='<repurl text="news:" />')
+    PROBLEM:  NotTag(what='<repurl text="http://friderwaves." />')
+    PROBLEM:  NotTag(what='<repurl text="http://store." />')
+    PROBLEM:  NotTag(what='<repurl text="http://www." />')
+    PROBLEM:  NotTag(what='<repurl text="http://www." />')
+    '''
+    # with open("./Données/dataset.csv", "r") as myfile:
+    with open("./Données/dataset.csv", "r") as myfile:
+        data = myfile.read() # Générait un NotTag(tests.that)
+    formatted = pretraitement.formattage(data)
+    tags = pretraitement.extract_tags(formatted)
+    for t in tags:
+        if (type(t) != treetaggerwrapper.Tag): print("PROBLEM: ", t)
+    assert all( (type(tag) is treetaggerwrapper.Tag) for tag in tags)
