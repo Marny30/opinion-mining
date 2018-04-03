fcfb4669bb379d15ad907aa595198f0d1832083b
Julien Marin <marin.julien@etu.umontpellier.fr>
2018-04-03 19:18:36 +0200

    optimisation pretraitement et génération de dataset prétraité

--- pretraitement.py
+++ pretraitement.py
@@ -2,6 +2,12 @@
 
 import treetaggerwrapper
 
+import os
+_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+_TREETAGGER_DIR = _ROOT_DIR+"/dep/treetagger"
+_TAGGER = treetaggerwrapper.TreeTagger(TAGLANG='en',
+                                       TAGDIR=_TREETAGGER_DIR)
+
 def extract_tags(sentence):
     '''Méthode d'extraction des tags (voir _extract_tags) avec exclusion
     des éléments n'ayant pas pu être taggués
@@ -14,12 +20,7 @@ def _extract_tags(sentence):
     ''' Méthode interne d'extraction des tags (Mot, classe grammaticale, lemme) depuis le
     treetaggerwrapper
     '''
-    import os
-    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-    TREETAGGER_DIR = ROOT_DIR+"/dep/treetagger"
-    tagger = treetaggerwrapper.TreeTagger(TAGLANG='en',
-                                          TAGDIR=TREETAGGER_DIR)
-    tagged_text = tagger.tag_text(sentence)
+    tagged_text = _TAGGER.tag_text(sentence)
     tags = treetaggerwrapper.make_tags(tagged_text)
     return tags
 
@@ -28,33 +29,40 @@ def formattage(sentence):
     res = sentence.lower()
     # Remplacer "." par ". "
     res = res.replace(".", ". ")
-    # On garde l'apostrophe pour les constructions du style "He has/He is => He's" ou pour le possessif
+    # On garde l'apostrophe pour les constructions du style "He has/He
+    # is => He's" ou pour le possessif
     ponctuation_to_keep = [',', '.', ';', '"', "'", ' ']
-    res = "".join(
-        [
+    res = "".join([
         letter for letter in sentence if
-        (letter.isalpha() or letter in ponctuation_to_keep)
-    ]
-    )
+        (letter.isalpha() or letter in ponctuation_to_keep)])
     return res
 
-def selection(tags):
-    ''' fonction de selection des tags 
-
-    liste des classes grammaticales : http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/Penn-Treebank-Tagset.pdf'''
+def pos_selection(tags):
+    ''' fonction de selection des tags
+    liste des classes grammaticales :
+    http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/Penn-Treebank-Tagset.pdf'''
     # interestingPOS=["VBG","NN","NNS","VBR","VBZ","JJS","RBS","VB","VBD","JJ","JJR","JJS","VBN"]
     # TODO : negation? RB
     interestingPOS = ["NN","NNS","JJS", "RBS","JJ","JJR","JJS"]
     res = []
     for tag in tags:
-        if (tag.pos  in  interestingPOS):
-            res+=[{'pos': tag.pos, 'lemma':tag.lemma}]
+        if (tag.pos in interestingPOS):
+            res += [{'pos': tag.pos, 'lemma':tag.lemma}]
     return res
 
-def pretraitement(sentence):
+
+def selection_lemma(selected):
+    return " ".join([x['lemma'] for x in selected])
+
+def pretraitement(sentence, lemmatize=True):
     formatted = formattage(sentence)
     tags = extract_tags(formatted)
-    return selection(tags)
+    selection = pos_selection(tags)
+    if lemmatize:
+        return selection_lemma(selection)
+    else:
+        return selection
+
 
 def Entree():
     import csv
@@ -67,31 +75,50 @@ def Entree():
             Text+=[csvfile.readline()]
     return Text
 
-def selection_lemma(selected):
-    return " ".join([x['lemma'] for x in selected])
-
 def main():
     ''' Étape 1 '''
-    import sys, csv
-    fichierSorti = open("datasetlematise.csv", "w")
-    texts = Entree()
-    for text in texts:
-        print(text)
-        print()
-        formatted = formattage(text)
-        tags = extract_tags(formatted)
-        select = selection(tags)
-        lemmas = selection_lemma(select)  # Texte lemmatisé
-        print(lemmas)
-        print()
-        print()
-        fichierSorti.write(lemmas+"\n")
-    fichierSorti.close()
-    # print(tags)
-    # select=selection(tags)
-    # print("Phrase filtrée et lemmatisée")
-    # lemmas = " ".join([x['lemma'] for x in select])
-    # print(lemmas)
+    import sys
+    out_path = "./Données/lemmatized_dataset.csv"
+    in_path = "./Données/dataset.csv"
+    data = [line.strip('\n') for line in open(in_path)]
+    chunksize = 10
+    indice = 0
+
+    # obtention du nb de lignes déjà prétraitées ##############################
+
+    try: indice = len([line.strip('\n') for line in open(out_path, "r")])
+    except: indice= 0
     
+    if indice > len(data):
+        print("[!] len(données_traitées) > len(dataset_donné)\n" +
+              "    Effacement du fichier de sortie.", file=sys.stderr)
+        myfile = open(out_path, "w+")
+        myfile.close()
+        indice = 0
+    elif indice == len(data):
+        print("le dataset est déjà prétraité! fermeture..", file=sys.stderr)
+        exit()
+
+
+    # prétraitement des données  ##############################################
+    print("pretraitement à partir de i =", indice, file=sys.stderr)
+    while indice != len(data):
+        # generation de chunksize avis lemmatisé ##################################
+        newdata = []
+        next_i = min(indice + chunksize, len(data))
+        for i in range(indice, next_i):
+            newdata += [pretraitement(data[i], lemmatize=True)]
+            indice += 1
+        print(indice, "/", len(data), file=sys.stderr)
+
+        # écriture ################################################################
+        print("Écriture..", end='', file=sys.stderr)
+        sys.stderr.flush()
+        with open(out_path, "a+") as lemma_file:
+            for line in newdata:
+                lemma_file.write(line+'\n')
+        print("\t ok", file=sys.stderr)
+        sys.stderr.flush()
+
 if __name__ == "__main__":
     main()
