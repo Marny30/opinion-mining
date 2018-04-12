d9819bd23dd80dd6c248dbade3da057983e591a7
Julien Marin <marin.julien@etu.umontpellier.fr>
2018-04-12 10:46:42 +0200

    mémorisation des prédictions (pour même jeu de donnée) dans le classifierwrapper

--- classifierwrapper.py
+++ classifierwrapper.py
@@ -4,7 +4,6 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
 #ref : http://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
-        
 class ClassifierWrapper:
     """ Classifieur paramétrable selon le type de classifieur souhaité
     """
@@ -12,25 +11,28 @@ class ClassifierWrapper:
         # TF-idf : Term Frequency times inverse document frequency
         self.clf = clf
         # print("entrainement sur " + len(data_train_tfidf) + " données ")
+        self.prediction = None
+        self.predictionid = None
         self.clf.fit(data_train_tfidf, labels)
-        
+
     def predict(self, data_test_tfdif):
+        """ Fonction de prédiction permettant de mémoriser une prédiction déjà
+        faite pour le même jeu de donnée
+        """
+        if self.prediction is None:
+            predictionid = hash(data_test_tfdif)
+            if predictionid != self.predictionid:
+                self.prediction = self.clf.predict(data_test_tfdif)
         return self.clf.predict(data_test_tfdif)
 
-    def visualization(self): # TODO
-        pass
-
-# note pour plus tard : pour n=3, dataset non lemmatisé plus précis, mais si n augmente cet écart s'amoindri
 def main():
     import utils
-
     classifier = KNeighborsClassifier(n_neighbors=4)
     # autre exemple
     # classifier = MultinomialNB()
-    # print(utils.FORMATTED_DATA_TRAIN['raw'])
     k_neighbors_classifier = ClassifierWrapper(utils.FORMATTED_DATA_TRAIN['raw'], utils.LABELS_TRAIN, classifier)
     prediction = k_neighbors_classifier.predict(utils.FORMATTED_DATA_TEST['raw'])
-    print("Précision pour kneighbors (n=3) : ", accuracy_score(utils.LABELS_TEST, prediction))
+    print("Accuracy pour kneighbors (n=3) : ", accuracy_score(utils.LABELS_TEST, prediction))
 
 if __name__ == '__main__':
     main()
