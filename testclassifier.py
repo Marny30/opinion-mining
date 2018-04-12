f0c5c78e06eabdf0bc9b191b207e7e28db28d84f
Julien Marin <marin.julien@etu.umontpellier.fr>
2018-04-12 19:01:41 +0200

    paramétrage pour jeu de test différent que celui d'entrée

--- testclassifier.py
+++ testclassifier.py
@@ -13,19 +13,17 @@ class ClassifierTester:
         self.label = label
         self.clf_metrics = {}
         self.clf_metrics['label'] = label
-        for dataset_type in utils.FORMATTED_DATA:
+        for dataset_type in utils._DATA:
             # raw, lemma, without stopwords, ....
             self.clf_metrics[dataset_type] = {}
-        self.clf = classifier
+        self.clfwrapper =  ClassifierWrapper(classifier)
         
     def compute_metrics(self):
-        for dataset_type in utils.FORMATTED_DATA:
+        for dataset_type in utils._DATA:
             training_start_time = time.time()
-            clfwrapper = ClassifierWrapper(
-                utils.FORMATTED_DATA_TRAIN[dataset_type],
-                utils.LABELS_TRAIN, self.clf)
+            self.clfwrapper.train(utils.FORMATTED_DATA_TRAIN[dataset_type], utils.LABELS_TRAIN)
             self.clf_metrics[dataset_type]['training_time'] = time.time() - training_start_time
-            self._compute_metrics(dataset_type, clfwrapper)
+            self._compute_metrics(dataset_type, self.clfwrapper)
         return self.clf_metrics
 
     def _compute_metrics(self, dataset_type, clfwrapper):
@@ -46,38 +44,43 @@ class ClassifierTester:
         # Temps d'analyse en seconde
         analysis_result['prediction_time'] = time.time() - analysis_start_time
         
-        
         # from sklearn.metrics import classification_report
         # print(self.label, "dataset : ", dataset_type)
-        # print(classification_report(ClassifierTester.labels_test, prediction))
-        
+        # print(classification_report(ClassifierTester.labels_test, prediction))        
         return analysis_result
 
     # TODO
-    # def get_comment_giving_wrong_predictions(self, dataset_type):
-    #     res = []
+    # def get_comment_giving_wrong_predictions(self, dataset_type, n=0):
+    #     formatted_data = utils.FORMATTED_DATA_TEST[dataset_type]
+    #     raw_data = utils._DATA_TEST[dataset_type]
+    #     res = {'false_positive' : [], 'false_negative' : []}
     #     t_labels = utils.LABELS_TEST
-    #     data = utils.FORMATTED_DATA_TEST[dataset_type]
-    #     prediction = self.clf.predict(data)
+    #     if n == 0:
+    #         n = len(utils.LABELS_TEST)  # nombre de donnée de test
+    #     # relance une prédiction..!
+    #     prediction = self.clfwrapper.predict(formatted_data)
         
     #     for i in range(len(t_labels)):
     #         if utils.LABELS_TEST[i] != prediction[i]:
-    #             res += data[i]
-    
+    #             if utils.LABELS_TEST[i] == 1:
+    #                 res['false_negative'] += [raw_data[i]]
+    #             elif utils.LABELS_TEST[i] == -1:
+    #                 res['false_positive'] += [raw_data[i]]
+    #             else:
+    #                 raise ValueError('Formattage utils.LABELS_TEST[i]')
+
+        # on ne retient que les n premiers avis
+        res['false_negative'] = res['false_negative'][:n]
+        res['false_positive'] = res['false_positive'][:n]
+        return res
+
 def main():
     from sklearn.neighbors import KNeighborsClassifier
     from sklearn.naive_bayes import MultinomialNB
     from sklearn.tree import DecisionTreeClassifier
     from sklearn import svm
-    labels = [
-        # "K Neighbors n=1",
-        # "K Neighbors n=5",
-        # "K Neighbors n=10",
-        # "Naive Bayes",
-        # "Decision Tree",
-        "SVM"
-    ]
-
+    
+    # TODO : jouer variation C
     classifiers = [
         ["K Neighbors n=1", KNeighborsClassifier(n_neighbors=1)],
         ["K Neighbors n=5", KNeighborsClassifier(n_neighbors=5)],
@@ -97,9 +100,22 @@ def main():
 
     import pprint
     pp = pprint.PrettyPrinter()
+    # for tester in testers:
+        # tester.compute_metrics()
+        # print(tester.label)
+        # for dataset_type in utils._DATA:
+        # dataset_type = 'raw'
+        # print("jeu de donnée :" , dataset_type)
+        # tester.clfwrapper.train(utils.FORMATTED_DATA_TRAIN[dataset_type], utils.LABELS_TRAIN)
+        # res = tester.get_comment_giving_wrong_predictions(dataset_type)
+        # for key in res:
+        #     print(key, res[key])
+            # pp.pprint(res)
+            # print(res)
+            # print()
     for tester in testers:
-        tester.compute_metrics()
+        tester.compute_metrics()    
         pp.pprint(tester.clf_metrics)
-        
+
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -1,49 +1,19 @@
 #!/usr/bin/python3
 
-# from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-DATASET_PATH = "./Données/dataset.csv"
-LABELS_PATH = "./Données/labels.csv"
+def getLabels(path):
+    return [int(line.strip('\n')) for line in open(path)]
+
+def getDataset(path):
+    return [line.strip('\n') for line in open(path)]
 
 SHUFFLED_DATASET_PATH = "./Données/dataset_shuffled.csv"
 SHUFFLED_LABELS_PATH = "./Données/labels_shuffled.csv"
 SHUFFLED_LEMMA_DATASET_PATH = "./Données/lemmatized_dataset_shuffled.csv"
 SHUFFLED_LEMMA_MORPHO_DATASET_PATH = "./Données/lemmatized_morpho_dataset_shuffled.csv"
 SHUFFLED_WITHOUT_STOPWORDS_DATASET_PATH = "./Données/dataset_without_stopwords_shuffled.csv"
-
-
-class DataAdaptater():
-    """Classe permettant de transformer des données bruts en données
-    tf-idf pour permettre l'analyse par les classifieurs
-
-    """
-    count_vect = CountVectorizer()
-    tfidf_transformer = TfidfTransformer()
-    def __init__(self):
-        pass
-    def adapt(self, dataset):
-        data_counts = DataAdaptater.count_vect.fit_transform(dataset)
-        # TF-idf : Term Frequency times inverse document frequency
-        data_tfidf = DataAdaptater.tfidf_transformer.fit_transform(data_counts)
-        return data_tfidf
-
-    
-def getLabels(path):
-    return [int(line.strip('\n')) for line in open(path)]
-
-def getDataset(path):
-    # path = "./datasetlematise.csv"
-    return [line.strip('\n') for line in open(path)]
-
-
-LABELS = {}
 LABELS = getLabels(SHUFFLED_LABELS_PATH)
-
-n = 5000                        # todo : 2/3 1/3
-LABELS_TRAIN = LABELS[:n]
-LABELS_TEST = LABELS[n:]
 
 # Les données bruts ne sont pas censés apporter d'information
 # intéressante pour le reste du projet (contrairement aux données
@@ -54,20 +24,74 @@ _DATA['without-stopwords'] = getDataset(SHUFFLED_WITHOUT_STOPWORDS_DATASET_PATH)
 _DATA['lemma-morpho'] = getDataset(SHUFFLED_LEMMA_MORPHO_DATASET_PATH)
 _DATA['lemma'] = getDataset(SHUFFLED_LEMMA_DATASET_PATH)
 
-_DATA_TRAIN = {}
-_DATA_TEST = {}
-for key in _DATA_TRAIN:
-    _DATA_TRAIN[key] = _DATA[key][:n] # les n premiers
-    _DATA_TEST[key] = _DATA[key][n:]  # les n derniers
-
-FORMATTED_DATA = {}
 FORMATTED_DATA_TRAIN = {}
 FORMATTED_DATA_TEST = {}
-_da  = DataAdaptater()
+LABELS_TRAIN = []
+LABELS_TEST = []
 
-def format_data():
+
+class DataAdaptater():
+    """Classe permettant de transformer des données bruts en données
+    tf-idf pour permettre l'analyse par les classifieurs
+
+    """
+    vectorizer = TfidfVectorizer()
+    # tfidfVectorizer est équivalent à un CountVectorizer suivi d'un tfidf_transformer
+    # ref : doc ( http://scikit-learn.org/stable/modules/feature_extraction.html#common-vectorizer-usage )
+
+    @staticmethod
+    def adapt_train(dataset):
+        return DataAdaptater.vectorizer.fit_transform(dataset)
+
+    @staticmethod
+    def adapt_test(dataset):
+        return DataAdaptater.vectorizer.transform(dataset)
+
+def define_train_data(dataset, dataset_type):
+    """ transforme un dataset (non formatté) et le défini comme dataset d'entrainement """
+    FORMATTED_DATA_TRAIN[dataset_type] = DataAdaptater.adapt_train(dataset)
+        
+def define_test_data(dataset, dataset_type):
+    """ transforme un dataset (non formatté) et le défini comme dataset de test """
+    FORMATTED_DATA_TEST[dataset_type] = DataAdaptater.adapt_test(dataset)
+
+def define_train_labels(labels):
+    global LABELS_TRAIN
+    LABELS_TRAIN = labels
+
+def define_test_labels(labels):
+    global LABELS_TEST
+    LABELS_TEST = labels
+
+def prediction_config(test_dataset_path):
+    """Définie la configuration pour la prédiction, où les labels
+    résultants sont inconnus. Le dataset d'entrainement n'est ici pas
+    le même que le dataset de test.
+    """
+    define_train_labels(LABELS)
+    text_test = getDataset(test_dataset_path)
     for key in _DATA:
-        FORMATTED_DATA[key] = _da.adapt(_DATA[key])
-        FORMATTED_DATA_TRAIN[key] = FORMATTED_DATA[key][:n]
-        FORMATTED_DATA_TEST[key] = FORMATTED_DATA[key][n:]
-format_data()
+        text_train = _DATA[key]
+        define_train_data(text_train, key)
+        # Jeu de donné inchangé, pour chaque entraînement distinct
+        define_test_data(text_test, key)
+        
+def default_config(partie_entrainement=0.66):
+    """Définie la configuration par défaut pour les sous-programmes. La
+    configuration par défault est la suivante : 
+    - Un seul fichier est lu.
+    - les n premières données sont utilisées pour l'entrainement, les n 
+    suivantes sont utilisées pour le test
+
+    partie_entrainement est défini par défaut de manière à respecter
+    la proportion 2/3 entrainement, 1/3 test.
+    """
+    n = int(partie_entrainement * len(_DATA['raw']))
+    define_train_labels(LABELS[:n])
+    define_test_labels(LABELS[n:])
+    for key in _DATA:
+        text_train = _DATA[key][:n]
+        text_test = _DATA[key][n:]
+        define_train_data(text_train, key)
+        define_test_data(text_test, key)
+default_config()
