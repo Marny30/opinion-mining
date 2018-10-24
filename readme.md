b276b6ce2d9791904e0a5e53d189ee35ee5b256b
Marny <marin.julien@etu.umontpellier.fr>
2018-10-24 05:24:34 +0300

    cleanup code et rédaction readme

--- readme.md
+++ readme.md
@@ -1,12 +1,26 @@
+Ce projet traite d'opinion mining et a été développé en M1 au pendant l'UE
+d'extraction des connaissances. Il est question de predire si un avis est
+positif ou negatif en fonction de son contenu et d'autres avis dont le label est
+connu. Le but de ce projet est de comparer les prétraitements et les
+classifieurs; de manière à déterminer le bon tuning dans le cadre du machine
+learning.
+
 # Prétraitement des données
-Le prétraitement des données se fait en amont et de manière différée à
-l'éxécution des classifieurs. Il s'agit de paramétrer le main de
-pretraitement.py et de généré les versions transformées des données,
-ici déjà présentes.
+Le prétraitement des données se de manière différée à l'éxécution des
+classifieurs. Il s'agit de paramétrer le main de pretraitement.py et de généré
+les versions transformées des données, ici déjà présentes.
 
 # Évaluation des classifieurs
 Il s'agit d'exécuter le fichier testclassifier.py. 2/3 des données
 sont utilisées pour l'entrainement, 1/3 pour deviner les données.
 
-# Entrainement des classifieurs et test sur un autre ensemble de données
-Il faut pour ce but exécuter test_unknown_data.py
+# Résultats
+Il ressort de ce projet que pour nos données et quelque, les avis bruts
+(rivalisant avec les données sans stopword) sont en moyenne meilleur que les
+autres types de prétraitements (lemmatisation, lemmatisation avec selection
+morho-syntaxique).
+
+
+# Entrainement des classifieurs et test sur un ensemble de données non étiquetté
+Il faut pour ce but exécuter test_unknown_data.py. Sur un ensemble de donné inconnu et fourni au travers du déroulement de ce projet
+F1-score de 92% d a pu être achevé grâce à la déterminisaison du meilleur tuning : meilleur pre-traitement, classifieurs et arguments:
--- testclassifier.py
+++ testclassifier.py
@@ -22,22 +22,20 @@ class ClassifierTester:
         for dataset_type in utils._DATA:
             # raw, lemma, without stopwords, ....
             self.clf_metrics[dataset_type] = {}
-        self.clfwrapper =  ClassifierWrapper(classifier)
         self.classifier = classifier
-        
+
     def compute_metrics(self):
         for dataset_type in utils._DATA:
             training_start_time = time.time()
-            self.clfwrapper.train(utils.FORMATTED_DATA_TRAIN[dataset_type], utils.LABELS_TRAIN)
+            self.classifier.fit(utils.FORMATTED_DATA_TRAIN[dataset_type], utils.LABELS_TRAIN)
             self.clf_metrics[dataset_type]['training_time'] = time.time() - training_start_time
-            self._compute_metrics(dataset_type, self.clfwrapper)
+            self._compute_metrics(dataset_type)
         return self.clf_metrics
 
-    def _compute_metrics(self, dataset_type, clfwrapper):
+    def _compute_metrics(self, dataset_type):
         analysis_start_time = time.time()
         analysis_result = self.clf_metrics[dataset_type]
-        prediction =  clfwrapper.predict(
-            utils.FORMATTED_DATA_TEST[dataset_type])
+        prediction =  self.classifier.predict(utils.FORMATTED_DATA_TEST[dataset_type])
         l_test = utils.LABELS_TEST
         cross_validation = cross_val_score(self.classifier, utils.FORMATTED_DATA_TRAIN[dataset_type], utils.LABELS_TRAIN, cv=10)
         analysis_result['cross_validation'] = cross_validation.mean()
@@ -87,42 +85,15 @@ class ClassifierTester:
 
         # from sklearn.metrics import classification_report
         # print(self.label, "dataset : ", dataset_type)
-        # print(classification_report(ClassifierTester.labels_test, prediction))        
+        # print(classification_report(ClassifierTester.labels_test, prediction))
         return analysis_result
-
-    # TODO
-    # def get_comment_giving_wrong_predictions(self, dataset_type, n=0):
-    #     formatted_data = utils.FORMATTED_DATA_TEST[dataset_type]
-    #     raw_data = utils._DATA_TEST[dataset_type]
-    #     res = {'false_positive' : [], 'false_negative' : []}
-    #     t_labels = utils.LABELS_TEST
-    #     if n == 0:
-    #         n = len(utils.LABELS_TEST)  # nombre de donnée de test
-    #     # relance une prédiction..!
-    #     prediction = self.clfwrapper.predict(formatted_data)
-        
-    #     for i in range(len(t_labels)):
-    #         if utils.LABELS_TEST[i] != prediction[i]:
-    #             if utils.LABELS_TEST[i] == 1:
-    #                 res['false_negative'] += [raw_data[i]]
-    #             elif utils.LABELS_TEST[i] == -1:
-    #                 res['false_positive'] += [raw_data[i]]
-    #             else:
-    #                 raise ValueError('Formattage utils.LABELS_TEST[i]')
-
-        # on ne retient que les n premiers avis
-        # res['false_negative'] = res['false_negative'][:n]
-        # res['false_positive'] = res['false_positive'][:n]
-        # return res
 
 def main():
     from sklearn.neighbors import KNeighborsClassifier
     from sklearn.naive_bayes import MultinomialNB
     from sklearn.tree import DecisionTreeClassifier
     from sklearn.linear_model import SGDClassifier
-    
-    
-    # TODO : jouer variation C
+
     classifiers = [
         ["K Neighbors n=1", KNeighborsClassifier(n_neighbors=1)],#
         ["K Neighbors n=5", KNeighborsClassifier(n_neighbors=5)],
@@ -136,29 +107,16 @@ def main():
         ["SVM rbf, gamma=1/5000", svm.SVC(kernel='rbf')],
         ["SGDClassifier", SGDClassifier()]
     ]
-    
+
     testers = []
     for couple_label_clf in classifiers:
         testers.append(ClassifierTester(couple_label_clf[0], couple_label_clf[1]))
 
     import pprint
     pp = pprint.PrettyPrinter()
-    # for tester in testers:
-        # tester.compute_metrics()
-        # print(tester.label)
-        # for dataset_type in utils._DATA:
-        # dataset_type = 'raw'
-        # print("jeu de donnée :" , dataset_type)
-        # tester.clfwrapper.train(utils.FORMATTED_DATA_TRAIN[dataset_type], utils.LABELS_TRAIN)
-        # res = tester.get_comment_giving_wrong_predictions(dataset_type)
-        # for key in res:
-        #     print(key, res[key])
-            # pp.pprint(res)
-            # print(res)
-            # print()
-   
+
     for tester in testers:
-        tester.compute_metrics()    
+        tester.compute_metrics()
         pp.pprint(tester.clf_metrics)
 
 
